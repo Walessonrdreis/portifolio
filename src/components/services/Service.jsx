543ded6c3bf957017b60e9ea12030b7f54d67b33
Walessonrdreis <wallllase@hotmail.com>
2022-07-27 07:12:36 -0300

    Finalizando Card.jxs e Card.css

--- src/components/services/Service.jsx
+++ src/components/services/Service.jsx
@@ -1,9 +1,61 @@
 import React,{useContext} from 'react';
 import "./Services.css";
+import { themeContext } from '../../Context';
+import Resume from "./resume.pdf";
+import {motion} from "framer-motion";
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.webp";
+import Glasses from "../../img/glasses.webp";
+import Humble from "../../img/humble.webp";
+
 
 const Service = () => {
+  // context 
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+  // transition ;
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+  
   return (
-    <div>Service</div>
+    <div className="services" id="services" >
+    {/* left side */}
+    <div className="awesome">
+      {/* darkMode */}
+      <span style={{color: darkMode ? "white": ""}}>My Awesome</span>
+      <span>services</span>
+      <span>lorem ipsum dolor sit amet, consectetur adip.
+      <br/>
+      lorem ipsum dolor sit amet.
+      </span>
+
+      <a href={Resume} download >
+        <button className="button s-button">Download CV</button>
+      </a>
+      <div className="blur s-blur1" style={{background: "#abf1ff94"}}></div>
+    </div>
+    {/* right */}
+    <div className="cards">
+      {/* first card */}
+      <motion.div
+      initial={{left: "25rem"}}
+      whileInView={{left: "14rem"}}
+      transition={transition}
+      >
+      <Card
+        emoji={HeartEmoji}
+        heading={"Design"}
+        detail={"Photoshop, Figma"}
+       />
+
+      </motion.div>
+
+    </div>
+
+    </div>
+
   )
 }
 
